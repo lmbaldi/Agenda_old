173d8a0eee04c0821b25f061a00e4f044363ab4a
marcelobaldi <marcelobaldi@celepar.pr.gov.br>
2020-11-23 20:51:18 -0300

    first commit

---
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,29 @@
+package com.example.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.agenda.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de Alunos");
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Caio", "Bianca", "Bruna", "Felipe", "Melissa", "Sofia"));
+
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, alunos));
+
+     }
+}
