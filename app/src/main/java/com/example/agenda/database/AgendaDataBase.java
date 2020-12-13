05b9b2cf149a42ba0e8ccaa1b9bfb2cff7b5c751
lmbadi <luizmarcelobaldi@hotmail.com>
2020-12-13 11:47:51 -0300

    add non-primitive object and saving to the bank

--- app/src/main/java/com/example/agenda/database/AgendaDataBase.java
+++ app/src/main/java/com/example/agenda/database/AgendaDataBase.java
@@ -6,13 +6,16 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.example.agenda.database.converter.ConversorCalendar;
 import com.example.agenda.database.dao.RoomAlunoDao;
 import com.example.agenda.modelo.Aluno;
 
-@Database(entities = {Aluno.class}, version = 3, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 4, exportSchema = false)
+@TypeConverters({ConversorCalendar.class})
 public abstract class AgendaDataBase extends RoomDatabase {
 
     private static final String NOME_BANCO_DE_DADOS = "agenda.db";
@@ -25,32 +28,37 @@ public abstract class AgendaDataBase extends RoomDatabase {
                 .databaseBuilder(context, AgendaDataBase.class, NOME_BANCO_DE_DADOS)
                 .allowMainThreadQueries()
                 .addMigrations(new Migration(1, 2) {
-                    @Override
-                    public void migrate(@NonNull SupportSQLiteDatabase database) {
-                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
-                    }
-                }, new Migration(2, 3) {
-                    @Override
-                    public void migrate(@NonNull SupportSQLiteDatabase database) {
-                        //criar uma nova tabela com as informacoes desejadas
-                        database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` " +
-                                "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                                "`nome` TEXT, " +
-                                "`telefone` TEXT, " +
-                                "`email` TEXT)");
+                                   @Override
+                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
+                                       database.execSQL("ALTER TABLE Aluno ADD COLUMN sobrenome TEXT");
+                                   }
+                               }, new Migration(2, 3) {
+                                   @Override
+                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
+                                       //criar uma nova tabela com as informacoes desejadas
+                                       database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` " +
+                                               "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                                               "`nome` TEXT, " +
+                                               "`telefone` TEXT, " +
+                                               "`email` TEXT)");
 
-                        //copiar dados da tabela antiga para a nova
-                        database.execSQL("INSERT INTO Aluno_novo (id, nome, telefone, email)" +
-                                "SELECT id, nome, telefone, email FROM Aluno" );
+                                       //copiar dados da tabela antiga para a nova
+                                       database.execSQL("INSERT INTO Aluno_novo (id, nome, telefone, email)" +
+                                               "SELECT id, nome, telefone, email FROM Aluno");
 
-                        //remover tabela antiga
-                        database.execSQL("DROP TABLE Aluno");
+                                       //remover tabela antiga
+                                       database.execSQL("DROP TABLE Aluno");
 
-                        //renomear a tabela nova com o nome da tabela antiga
-                        database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
-                    }
-                }
-             ).build();
+                                       //renomear a tabela nova com o nome da tabela antiga
+                                       database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
+                                   }
+                               }, new Migration(3,4) {
+                                   @Override
+                                   public void migrate(@NonNull SupportSQLiteDatabase database) {
+                                       database.execSQL("ALTER TABLE Aluno ADD COLUMN momentoDeCadastro INTEGER");
+                                   }
+                               }
+                ).build();
     }
 
 }
--- app/src/main/java/com/example/agenda/modelo/Aluno.java
+++ app/src/main/java/com/example/agenda/modelo/Aluno.java
@@ -6,6 +6,8 @@ import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Entity
 public class Aluno implements Serializable {
@@ -15,13 +17,14 @@ public class Aluno implements Serializable {
     //private  String sobreNome;
     private  String telefone;
     private  String email;
+    private Calendar momentoDeCadastro = Calendar.getInstance();
 
     @Ignore
-    public Aluno(String nome,  String telefone, String email) {
+    public Aluno(String nome, String telefone, String email) {
         this.nome = nome;
-      //  this.sobreNome = sobreNome;
         this.telefone = telefone;
         this.email = email;
+      //  this.sobreNome = sobreNome;
     }
 
     public Aluno() {}
@@ -42,14 +45,22 @@ public class Aluno implements Serializable {
         this.nome = nome;
     }
 
-   /* public String getSobreNome() {
-        return sobreNome;
+    public Calendar getMomentoDeCadastro() {
+        return momentoDeCadastro;
     }
 
-    public void setSobreNome(String sobreNome) {
-        this.sobreNome = sobreNome;
+    public void setMomentoDeCadastro(Calendar momentoDeCadastro) {
+        this.momentoDeCadastro = momentoDeCadastro;
     }
-*/
+
+    /* public String getSobreNome() {
+            return sobreNome;
+        }
+
+        public void setSobreNome(String sobreNome) {
+            this.sobreNome = sobreNome;
+        }
+    */
     public String getTelefone() {
         return telefone;
     }
@@ -78,5 +89,10 @@ public class Aluno implements Serializable {
 
     public String getNomeCompleto() {
         return nome ;
+    }
+
+    public String dataFormatada(){
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        return formatador.format(momentoDeCadastro.getTime());
     }
 }
